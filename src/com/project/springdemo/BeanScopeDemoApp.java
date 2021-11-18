75d5a54935bfefdd000599fad9a79c4172f318e3
Jatin-Suthar <jatinsuthar300@gmail.com>
2021-11-18 15:24:36 +0530

    all files successfully added

--- src/com/project/springdemo/BeanScopeDemoApp.java
+++ src/com/project/springdemo/BeanScopeDemoApp.java
@@ -5,15 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BeanScopeDemoApp {
 	public static void main(String[] args) {
 		//load the spring config files
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 		//retrieve beans from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach alphaCoach = context.getBean("myCoach", Coach.class);
-		//check if they are the same beans
-		boolean compare = (theCoach == alphaCoach);
-		System.out.println("Quick check whether they pointing to the same memory location:  " + compare);
-		System.out.println("Memory location for first object:  " + theCoach);
-		System.out.println("Memory loaction for second object:  " + alphaCoach);
+		Coach coach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(coach.getDailyWorkout());
+		//close the context
 		context.close();
 	}
 }
--- src/com/project/springdemo/TrackCoach.java
+++ src/com/project/springdemo/TrackCoach.java
@@ -15,4 +15,13 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It! "+theFortuneService.getFortune();
 	}
+	
+	//add an init method:
+	public void doMyStartupStuff() {
+		System.out.println("Track Coach: Do my startupStuff");
+	}
+	//add an destroy method:
+	public void doMyCleanupStuff() {
+		System.out.println("Tack Coach: Do my cleanupStuff");
+	}
 }
